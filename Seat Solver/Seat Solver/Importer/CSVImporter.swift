069ebb5c25c390faaab6924a94f00875a55d70d7
Christian Bieniak <cbieniak@gmail.com>
2018-07-15 22:05:18 +1000

    CSVImporter will Takes a CSV file and extracts the data into a JSON format.

---
+++ Seat Solver/Seat Solver/Importer/CSVImporter.swift
@@ -0,0 +1,10 @@
+//
+//  CSVImporter.swift
+//  Seat Solver
+//  Takes a CSV file and extracts the data into a JSON format.
+//
+//  Created by Christian Bieniak on 15/7/18.
+//  Copyright © 2018 Christian Bieniak. All rights reserved.
+//
+
+import Foundation
