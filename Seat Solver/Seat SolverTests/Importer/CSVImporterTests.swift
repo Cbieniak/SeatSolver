9acd788c84ea808d2f157093f4bc317d4fe71dc8
Christian Bieniak <cbieniak@gmail.com>
2018-07-15 22:07:02 +1000

    add CSVImporterTests

---
+++ Seat Solver/Seat SolverTests/Importer/CSVImporterTests.swift
@@ -0,0 +1,14 @@
+//
+//  CSVImporterTests.swift
+//  Seat SolverTests
+//
+//  Created by Christian Bieniak on 15/7/18.
+//  Copyright © 2018 Christian Bieniak. All rights reserved.
+//
+
+import XCTest
+@testable import Seat_Solver
+
+class CSVImporterTests: XCTestCase {
+    
+}
