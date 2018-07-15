20dfe9603774357c990395dac877101d1182a593
Christian Bieniak <cbieniak@gmail.com>
2018-07-15 22:47:53 +1000

    testExtracting data from CSV

--- Seat Solver/Seat Solver/Importer/CSVImporter.swift
+++ Seat Solver/Seat Solver/Importer/CSVImporter.swift
@@ -9,12 +9,34 @@
 
 import Foundation
 
+enum CSVImporterError: Error {
+    case fileNotFound
+    case invalidFormat
+}
+
 struct CSVImporter {
-    static func urlForCSVFile(named filename: String, bundle: Bundle) -> URL? {
-        return bundle.url(forResource: filename, withExtension: "csv")
+    
+    static let csvExtension = "csv"
+    
+    static func extractData(from fileName: String, bundle: Bundle) throws -> [[String]] {
+        let url = try urlForCSVFile(named: fileName, bundle: bundle)
+        let data = try Data(contentsOf: url)
+        return try stringsFromData(data: data)
     }
     
-//    static func extractData(from fileName: String, bundle: Bundle) -> Data {
-//
-//    }
+    static func urlForCSVFile(named filename: String, bundle: Bundle) throws -> URL {
+        if let result = bundle.url(forResource: filename, withExtension: csvExtension) {
+            return result
+        } else {
+            throw CSVImporterError.fileNotFound
+        }
+    }
+    
+    static func stringsFromData(data: Data) throws -> [[String]] {
+        guard let stringData = String(data: data, encoding: .utf8) else { throw CSVImporterError.invalidFormat }
+        let outerArrays: [String] = stringData.split(separator: "\r\n").map { String($0) }
+        return outerArrays.map { $0.split(separator: ",").map({ String($0) }) }
+    }
+    
+    
 }
--- Seat Solver/Seat SolverTests/Importer/CSVImporterTests.swift
+++ Seat Solver/Seat SolverTests/Importer/CSVImporterTests.swift
@@ -11,7 +11,21 @@ import XCTest
 
 class CSVImporterTests: XCTestCase {
     
-    func testItRetrievesTheCorrectURL() {
-        XCTAssertNotNil(CSVImporter.urlForCSVFile(named: "DemoData", bundle: Bundle(for: self.classForCoder)))
+    func testExtractData() {
+        XCTAssertNoThrow(try CSVImporter.extractData(from: "DemoData", bundle: Bundle(for: self.classForCoder)))
+        
+        let data = try! CSVImporter.extractData(from: "DemoData", bundle: Bundle(for: self.classForCoder))
+        
+        XCTAssertTrue(data.count == 5)
+        XCTAssertTrue(data[0].count == 3)
+        
+    }
+    
+    func testItRetrievesTheURL() {
+        XCTAssertNoThrow(try CSVImporter.urlForCSVFile(named: "DemoData", bundle: Bundle(for: self.classForCoder)))
+    }
+    
+    func testItThrowsErrorWhenFileDoesntExist() {
+        XCTAssertThrowsError(try CSVImporter.urlForCSVFile(named: "DoesntExist", bundle: Bundle(for: self.classForCoder)))
     }
 }
